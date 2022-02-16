2d70f91e21667ad4196e6546b1c6a1809487a6ad
toineto <augusto.vilhena4@gmail.com>
2022-02-16 20:50:58 -0300

    adiciona alterações no settings

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -3,8 +3,8 @@ import React, { Component } from 'react';
 export default class Settings extends Component {
   render() {
     return (
-      <div data-testid="settings-title">
-        <h1>Settings</h1>
+      <div data-testid="settings-title" className="settings">
+        <h1 className="headerSettings">Settings</h1>
       </div>
     );
   }
