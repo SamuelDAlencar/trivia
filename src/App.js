d3843f26f0e6ad8ec42101ab37ee39f6bd925acd
SamuelDAlencar <sarl.2001@hotmail.com>
2022-02-17 11:21:15 -0300

    fix: folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import Game from './pages/Game';
-import Settings from './components/Settings';
+import Settings from './pages/Settings';
 import Feedback from './pages/Feedback';
 import Ranking from './pages/Ranking';
 import './App.css';
