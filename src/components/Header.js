b7e7c8dfc94338a562cfe1c492ce6febdf639eda
SamuelDAlencar <sarl.2001@hotmail.com>
2022-02-17 17:35:58 -0300

    fix: log removal

--- src/components/Header.js
+++ src/components/Header.js
@@ -25,7 +25,7 @@ class Header extends Component {
   render() {
     const { name, score } = this.props;
     const { emailHash } = this.state;
-    console.log(score);
+
     return (
       <header className="header">
         <img
