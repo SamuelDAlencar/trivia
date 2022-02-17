64279db378d1da0750d0176124049ed19e0bb549
SamuelDAlencar <sarl.2001@hotmail.com>
2022-02-17 17:08:42 -0300

    feat: requirement 18

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
 import propTypes from 'prop-types';
 import { connect } from 'react-redux';
+import md5 from 'crypto-js/md5';
 import { playerAction, scoreAction } from '../redux/actions';
 
 class Ranking extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      ranking: [],
+    };
+  }
+
+  componentDidMount() {
+    this.handleRanking();
+  }
+
+  handleRanking = async () => {
+    const { name, score, picture } = this.props;
+
+    if (name) {
+      if (localStorage.getItem('ranking')) {
+        localStorage.setItem('ranking', JSON.stringify(
+          [
+            ...JSON.parse(localStorage.getItem('ranking')),
+            {
+              name, score, picture,
+            },
+          ],
+        ));
+      } else {
+        localStorage.setItem('ranking', JSON.stringify(
+          [
+            {
+              name, score, picture,
+            },
+          ],
+        ));
+      }
+    }
+    this.setState({ ranking: JSON.parse(localStorage.getItem('ranking')) });
+  }
+
   goHome = () => {
     const { history, resetGame } = this.props;
     resetGame(0, 0, {});
@@ -11,9 +50,22 @@ class Ranking extends Component {
   }
 
   render() {
+    const { ranking } = this.state;
     return (
       <>
         <h1 data-testid="ranking-title">Ranking</h1>
+        <ul className="ranking-players">
+          {ranking.sort((a, b) => b.score - a.score).map((player, i) => (
+            <li key={ i }>
+              <h4 data-testid={ `player-name-${i}` }>
+                {player.name}
+              </h4>
+              <h4 data-testid={ `player-score-${i}` }>
+                {player.score}
+              </h4>
+            </li>
+          ))}
+        </ul>
         <button
           type="button"
           data-testid="btn-go-home"
@@ -21,7 +73,6 @@ class Ranking extends Component {
         >
           Home
         </button>
-
       </>
     );
   }
@@ -33,8 +84,10 @@ Ranking.propTypes = {
 }.isRequired;
 
 const mapStateToProps = (state) => ({
+  name: state.player.name,
   score: state.player.score,
   assertions: state.player.assertions,
+  picture: `${md5(state.player.assertions).toString()}`,
 });
 
 const mapDispatchToProps = (dispatch) => ({
